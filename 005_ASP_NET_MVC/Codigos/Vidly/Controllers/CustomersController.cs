9d3745e07f050e91eb33d49ce5797bac3535cdef
lfzarazuaa <lfzarazuaa1400@outlook.com>
2020-11-03 09:28:28 -0600

    Optimizaciones.

--- 005_ASP_NET_MVC/Codigos/Vidly/Controllers/CustomersController.cs
+++ 005_ASP_NET_MVC/Codigos/Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -26,6 +27,13 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
+           if(MemoryCache.Default["Genres"] == null)
+           {
+               MemoryCache.Default["Genres"] = db.Genres.ToList();
+           }
+
+           var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
+
            return View();
         }
 
